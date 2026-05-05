365fd7141608654ab5a8b6ba55b5f35896e61b18
Zedocone <druezinsilva440@gmail.com>
2026-05-05 09:21:56 -0300

    Enhance IMC calculator with more categories
    Added additional IMC categories for overweight and obesity levels.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("calculadora IMC 📱")
-st.subheader("feito com streamlit betinha mogado 🤣")
+st.subheader("feito com streamlit ✨")
 
 altura = st.number_input("digite a sua altura", min_value=0.0)
 peso = st.number_input("digite o seu peso",min_value=0.0)
@@ -9,8 +9,14 @@ peso = st.number_input("digite o seu peso",min_value=0.0)
 if st.button("calcular"):
     imc = peso / altura ** 2
     if imc < 18.5:
-        st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwyMUegwx-DTXnZ-H2pl0tC3eW-SDfba_u9w&s", width=150)
         st.error("abaixo do peso")
-    elif imc < 24.9:    
-        st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3vir_HIlJ-I_NCY8rHwaIa_M002KG4Ofo1g&s", width=150)
+    elif imc < 24.9:
         st.success("na media paizao")
+    elif imc < 29.9:
+        st.error("sobre peso")
+    elif imc 34.9:
+        st.error("obesidade grau 1")
+    elif imc 39.9:
+         st.error("obesidade grau 2")
+    elif imc > 40.0:
+         st.error("CUIDADO, risco de morte")
